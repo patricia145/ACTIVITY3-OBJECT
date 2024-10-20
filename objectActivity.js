99e57aa80c3a5141e67302ae84e024d1a580500e
patricia145 <patriciavaldez0517@gmail.com>
2024-10-20 20:34:13 +0800

    initial commit

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "hatchback",
+    model: "Ford Focus",
+    color: "Blue"
+};
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
